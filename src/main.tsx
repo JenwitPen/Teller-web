cd64d08dbdcc59a8c04b3dc300a181bea1ad4b4a
JenwitPen <jenwit_penjamrat@hotmail.com>
2026-03-17 21:02:06 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import { ConfigProvider } from 'antd';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider theme={themeConfig}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ConfigProvider>
   </React.StrictMode>
 );
